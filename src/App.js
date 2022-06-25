ab9ffd2f9fa666d9f840db5a859e848f07371375
saveliyB <saveliy199757@gmail.com>
2022-06-25 10:57:59 +0300

    Перенос верстки по компонентам index.html

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import './scss/app.scss';
+import Header from "./componets/Header";
+import Categories from "./componets/Categories";
+import Sort from "./componets/Sort";
+import PizzaBlock from "./componets/PizzaBlock";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="wrapper">
+         <Header />
+        <div className="content">
+          <div className="container">
+            <div className="content__top">
+              <Categories/>
+              <Sort />
+            </div>
+            <h2 className="content__title">Все пиццы</h2>
+            <div className="content__items">
+              <PizzaBlock />
+              <PizzaBlock />
+              <PizzaBlock />
+              <PizzaBlock />
+              <PizzaBlock />
+              <PizzaBlock />
+              <PizzaBlock />
+              <PizzaBlock />
+              <PizzaBlock />
+            </div>
+          </div>
+        </div>
+      </div>
   );
 }
 
